5f164bd4c47baac7ba3e997e5966ae12632cf795
erniechau0930 <pyechau@connect.ust.hk>
2026-04-24 19:37:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ option = st.selectbox("Choose your favorite color:",
                       ["Red", "Blue", "Green"])
 st.write(f"You selected: {option}")
 
+if st.button("Click Me"):
+    st.write("Button clicked!")
 
