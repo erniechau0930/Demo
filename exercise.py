57e4a22973f8e8c416fd02e612ab06d88bf0e2b0
erniechau0930 <pyechau@connect.ust.hk>
2026-04-28 22:31:01 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -6,7 +6,8 @@ st.title("Streamlit Exercise")
 with st.form(key = "my_form"):
   first_name = st.text_input("Enter your first name:")
   last_name = st.text_input("Enter your last name:")
-  submitted = st.form_submit_button("Submit")
+  favorite_number = st.number_inout("Enter your favorite number:")
+  submitted = st.form_submit_button("Register")
   if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
-st.write("Outside the form")
+        st.success
+
