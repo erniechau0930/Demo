3d9bd22a4ff5f52adab8a1558212e4581bc83fcf
erniechau0930 <pyechau@connect.ust.hk>
2026-04-28 22:46:29 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import panda as pd
+import os
 
 
 st.title("Contact Info Collector App")
@@ -6,16 +8,9 @@ st.write("This app helps you collect Contact Info")
 st.write("Please fill in the following form:")
 
 with st.form(key = "my_form"):
-  
   first_name = st.text_input("Enter your first name:")
-  if first_name == "":
-    st.warning("The empty is empty, please enter your first name!")
-    
   last_name = st.text_input("Enter your last name:")
-  if last_name == "":
-    st.warning("The empty is empty, please enter your last name!")
-
-  favorite_number = st.number_input("Enter your favorite number:")
+  favorite_number = st.number_input("Enter your favorite number:", step=1)
   
   if st.form_submit_button("Register"):
     st.success(f"Registration Success,Thank you!")
