99d57799defcfc1692a47f9fcc9052346c39a1df
erniechau0930 <pyechau@connect.ust.hk>
2026-04-28 22:42:34 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -8,11 +8,9 @@ st.write("Please fill in the following form:")
 with st.form(key = "my_form"):
   first_name = st.text_input("Enter your first name:")
   if first_name == "":
-  else:
     st.warning("The empty is empty, please enter your first name!")
   last_name = st.text_input("Enter your last name:")
-  if last_name == "":
-  else:
+  if last_name == "":s
     st.warning("The empty is empty, please enter your last name!")
   favorite_number = st.number_input("Enter your favorite number:")
 
