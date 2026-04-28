05bc2b1ddc23d9064bf16d5697644b32f097d16e
erniechau0930 <pyechau@connect.ust.hk>
2026-04-28 22:41:14 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -6,12 +6,12 @@ st.write("This app helps you collect Contact Info")
 st.write("Please fill in the following form:")
 
 with st.form(key = "my_form"):
-  first_name = st.text_input("Enter your first name:"):
-  if first_name == ""
+  first_name = st.text_input("Enter your first name:")
+  if first_name == "":
   else:
     st.warning("The empty is empty, please enter your first name!")
   last_name = st.text_input("Enter your last name:")
-  if last_name == ""
+  if last_name == "":
   else:
     st.warning("The empty is empty, please enter your last name!")
   favorite_number = st.number_input("Enter your favorite number:")
