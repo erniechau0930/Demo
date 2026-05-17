8f10b81a1a8cee46223074a38ab178349c6ac81f
erniechau0930 <pyechau@connect.ust.hk>
2026-05-18 00:53:14 +0800

    Update final_revision.py

--- final_revision.py
+++ final_revision.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 
 st.title("Isom 3400 Final Revision")
-st.header("Exam Date: 21st May, 12:30pm")
-st.subheader("Location: LTA")
 st.title("This is st.title()")
 st.header("This is st.header()")
 st.subheader("This is st.subheader()")
