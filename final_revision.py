c6f3280d156de34b2f1e28863fecc4b2fa4d2ccb
erniechau0930 <pyechau@connect.ust.hk>
2026-05-17 23:58:25 +0800

    Update final_revision.py

--- final_revision.py
+++ final_revision.py
@@ -20,7 +20,7 @@ st.write("""
 """)
 st.write(""" * Item 1""")
 
-""""""st.file_uploader()
+"st.file_uploader()
 st.tabs()
 st.columns()
 st.table()
@@ -35,4 +35,4 @@ st.button()
 st.form()
 st.bar_chart()
 st.form_submit_button()
-option_menu(menu_title= “”, options= [], icons=[], default_index=0)""""""
+option_menu(menu_title= “”, options= [], icons=[], default_index=0)"
