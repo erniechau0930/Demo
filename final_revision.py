3d4daaadf8d7fd8a43b70355483ddcfb2c3395d8
erniechau0930 <pyechau@connect.ust.hk>
2026-05-18 17:23:25 +0800

    Update final_revision.py

--- final_revision.py
+++ final_revision.py
@@ -14,7 +14,7 @@ st.subheader("st.subheader() - Subsection header")
 st.write("st.write() - Generic text (most flexible)")
 st.markdown("st.markdown() - You can make text **bold** with ** text **, *italicized* with * text *, or ~~strikethrough~~ with ~~ text ~~")
 st.markdown("""
-* Bullet points with st.markdown()
+* Bullet points with st.markdown(* text) 
 * Second bullet point
 """)
 
