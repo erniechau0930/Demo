5c274a2dfd33dfdac7e0add1b1158e2117d58b3c
erniechau0930 <pyechau@connect.ust.hk>
2026-05-18 17:50:38 +0800

    Update final_revision.py

--- final_revision.py
+++ final_revision.py
@@ -68,7 +68,7 @@ st.divider()
 # LAYOUT: COLUMNS
 # ============================================
 st.header("st.columns()")
-col1, col2, col3 = st.columns(4)
+col1, col2, col3, col4= st.columns(4)
 st.write("st.columns(number) - Creates side-by-side columns, returns column objects")
 
 with col1:
